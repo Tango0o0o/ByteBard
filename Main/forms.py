4ee27b820e1ae5cf9ed26b30bfb6e8196e3b8538
Tango0o0o <dacostawalanyo49@gmail.com>
2024-01-23 19:55:48 +0000

    Added flexbox to registration page, started designing it, also cleaned up base.css and switched to new font

--- Main/forms.py
+++ Main/forms.py
@@ -5,13 +5,16 @@ from django import forms
 from django.contrib.auth.models import User
 
 class CreateUserForm(UserCreationForm):
-    first_name = forms.CharField(max_length=30)
-    last_name = forms.CharField(max_length=30)
+    first_name = forms.CharField(max_length=30, label="First name")
+    last_name = forms.CharField(max_length=30, label="Last name")
 
     def __init__(self, *args: Any, **kwargs: Any) -> None:
         super().__init__(*args, **kwargs)
         self.fields["first_name"].label = "First name"
         self.fields["last_name"].label = "Last name"
+
+        for field in self.fields:
+            self.fields[field].widget.attrs.update({"class" : "form-input", "placeholder" : f"{field.capitalize()}"})
     
     class Meta:
         model = User
