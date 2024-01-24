129174fc51d6e9cc502f06177604d58e7ff3d9e3
Tango0o0o <93527311+Tango0o0o@users.noreply.github.com>
2024-01-24 14:56:48 +0000

    Update forms.py (added commemts)

--- Main/forms.py
+++ Main/forms.py
@@ -5,17 +5,19 @@ from django import forms
 from django.contrib.auth.models import User
 
 class CreateUserForm(UserCreationForm):
+    #adding fields
     first_name = forms.CharField(max_length=30, label="First name")
     last_name = forms.CharField(max_length=30, label="Last name")
 
     def __init__(self, *args: Any, **kwargs: Any) -> None:
         super().__init__(*args, **kwargs)
+        # labels for fields i added
         self.fields["first_name"].label = "First name"
         self.fields["last_name"].label = "Last name"
 
         for field in self.fields:
-            self.fields[field].widget.attrs.update({"class" : "form-input", "placeholder" : f"{field.capitalize()}"})
+            self.fields[field].widget.attrs.update({"class" : "form-input", "placeholder" : f"{field.capitalize()}"}) # adding the class "form-input" to every field, to allow CSS changes to be made. Also added placeholder text for the input box
     
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', "password2", "first_name", "last_name"]
+        fields = ['username', 'email', 'password1', "password2", "first_name", "last_name"] # Adding the fields
