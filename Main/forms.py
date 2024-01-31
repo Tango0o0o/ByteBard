677406983a7afcda3451b4270c298ec7373b8150
Tango0o0o <dacostawalanyo49@gmail.com>
2024-01-31 20:30:58 +0000

    Polished the form even more, removed password confirmation, added more comments to css

--- Main/forms.py
+++ Main/forms.py
@@ -16,8 +16,8 @@ class CreateUserForm(UserCreationForm):
         self.fields["last_name"].label = "Last name"
 
         for field in self.fields:
-            self.fields[field].widget.attrs.update({"class" : "form-input", "id" : f"{field}"}) # adding the class "form-input" to every field, to allow CSS changes to be made. Also added placeholder text for the input box
+            self.fields[field].widget.attrs.update({"class" : "form-input", "id" : f"{field}", "required" : True}) # adding the class "form-input" to every field, to allow CSS changes to be made. Also added placeholder text for the input box
     
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', "password2", "first_name", "last_name"] # Adding the fields
+        fields = ['username', 'email', 'password1', "first_name", "last_name"] # Adding the fields
